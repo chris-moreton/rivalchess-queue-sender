ccab55a29b687a70c78e7796d037e8ca0d986720
Chris Moreton <chris@netsensia.com>
2020-10-20 20:56:26 +0100

    Modify payload

--- src/main/java/com/netsensia/rivalchess/generator/MatchRequestPayload.kt
+++ src/main/java/com/netsensia/rivalchess/generator/MatchRequestPayload.kt
@@ -1,9 +1,13 @@
 package com.netsensia.rivalchess.generator
 
+data class EngineSettings(
+    val version: String,
+    val maxNodes: Int,
+    val maxMillis: Int,
+    val openingBook: String
+)
+
 data class MatchRequestPayload(
-    val engine1: String,
-    val engine2: String,
-    val nodesToSearch: Int?,
-    val maxTimeMillis: Int?,
-    val openingLibrary: String?
+    val engine1: EngineSettings,
+    val engine2: EngineSettings
 )
