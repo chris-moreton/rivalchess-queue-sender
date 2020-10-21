a8501985b37703318a4ee5c79ab28ba7eb185a50
Chris Moreton <chris@netsensia.com>
2020-10-21 23:10:16 +0100

    Add health endpoint

--- src/main/java/com/netsensia/rivalchess/recorder/GeneratorController.kt
+++ src/main/java/com/netsensia/rivalchess/recorder/GeneratorController.kt
@@ -5,6 +5,7 @@ import com.netsensia.rivalchess.vie.model.EngineSetting
 import com.netsensia.rivalchess.vie.model.EngineSettings
 import com.netsensia.rivalchess.vie.model.MultiMatch
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -45,5 +46,10 @@ class GreetingController {
                 matchRequestPayload.matchCount)
 
         return ResponseEntity.accepted().body(MatchResponsePayload())
+    }
+
+    @GetMapping("/health")
+    fun health(): ResponseEntity<String> {
+        return ResponseEntity.ok().body("ok")
     }
 }
